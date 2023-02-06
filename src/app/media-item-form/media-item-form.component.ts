1150e7942e5ac0f450353a3e7e8ce1aaa7ae2c61
irines <irina.kiselyowa12@gmail.com>
2023-02-06 22:30:09 +0200

    Custom validation function. 4_05.

--- src/app/media-item-form/media-item-form.component.ts
+++ src/app/media-item-form/media-item-form.component.ts
@@ -19,8 +19,23 @@ export class MediaItemFormComponent implements OnInit {
         Validators.required
       ])),
       category: new FormControl(''),
-      year: new FormControl('')
+      year: new FormControl('', this.yearValidator)
     });
+  }
+
+  // if null is return - the field is valid, else the object should be returned (it is available on the form.errors property)
+  yearValidator(control: FormControl) {
+    if (control.value.trim().length === 0) {
+      return null;
+    }
+    const year = parseInt(control.value)
+    const minYear = 1900;
+    const maxYear = 2100;
+    if (year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return { year: true };
+    }
   }
 
   onSubmit(mediaItem) {
