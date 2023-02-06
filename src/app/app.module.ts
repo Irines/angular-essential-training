c758ae77f5c95dbd6109d83b46740e0310021d87
irines <irina.kiselyowa12@gmail.com>
2023-02-06 22:03:37 +0200

    Model-Driven form approach. 4_03.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { MediaItemListComponent } from './media-item-list/media-item-list.compon
 import { FavoriteDirective } from "./favorite.directive";
 import { CategoryListPipe } from "./category-list.pipe";
 import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
-import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, ReactiveFormsModule],
   declarations: [AppComponent, MediaItemComponent, MediaItemListComponent, FavoriteDirective, CategoryListPipe, MediaItemFormComponent],
   bootstrap: [AppComponent],
 })
