de65aa8fdeebc7ca13b17549c71bae704f872da4
irines <irina.kiselyowa12@gmail.com>
2023-02-09 15:54:32 +0200

    HAdling http errors. 6_06.

--- src/app/services/media-item.service.ts
+++ src/app/services/media-item.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
 import { MediaItemResponse } from '../interfaces/media';
+import { throwError } from 'rxjs';
 
 // it tells Angular to make an instance of this service to use in this NgModule and it's children modules.
 @Injectable({
@@ -21,14 +22,24 @@ export class MediaItemService {
     // или преобразования и управления самими Observable используются специальные функции - операторы,
     // они указываются в методе pipe()
     return this.http.get<MediaItemResponse>('mediaitems', getOptions)
-    .pipe(map((response: MediaItemResponse) => {return response.mediaItems; }))
+    .pipe(
+      map((response: MediaItemResponse) => {return response.mediaItems; }),
+      catchError(this.handleError)
+    )
   }
 
   add(mediaItem) {
     return this.http.post('mediaitems', mediaItem)
+    .pipe(catchError(this.handleError))
   }
 
   delete(mediaItem) {
     return this.http.delete(`mediaitems/${mediaItem.id}`)
+    .pipe(catchError(this.handleError))
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log("Error: ", error.message)
+    return throwError("A data error occured. Please try again.")
   }
 }
